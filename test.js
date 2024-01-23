b9e6e711d3e81c6bbe888b72da66d3ab7b713b85
entttom <entner.tom@gmail.com>
2024-01-23 15:49:37 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+exec('xset dpms force on', (error, stdout, stderr) => {if (error) {return;}}); // Turn on Screen
+
+  exec('vlc rtsp://192.168.1.1:7447/6OHQ0QIWgxnIbTTp --fullscreen', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`)
+  
+});
+
+const time = 30; // time after streaming stops
+
+setTimeout(() => {
+    exec('killall -9 vlc', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+    
+    exec('xset dpms force off', (error, stdout, stderr) => {if (error) {return;}}); // Turn off Screen
+    
