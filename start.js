c85dc096a4c0b67160941b6b129ce543bfd81abe
entttom <entner.tom@gmail.com>
2024-01-23 12:12:00 +0100

    Update start.js

--- start.js
+++ start.js
@@ -10,21 +10,49 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+exec('xset dpms force off', (error, stdout, stderr) => {if (error) {return;}}); // Turn off Screen
 
 app.post('/api/ring_ring', (req, res) => {
 
-exec('ls ./', (err, output) => {
-    // once the command has completed, the callback function is called
-    if (err) {
-        // log and return if we encounter an error
-        console.error("could not execute command: ", err)
-        return
+
+//start vlc, check ifts already running
+exec('if pgrep vlc > /dev/null; then echo "VLC is already running"; else vlc & fi', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
     }
-    // log the output received from the command
-    console.log("Output: \n", output)
-})
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
 
+exec('xset dpms force on', (error, stdout, stderr) => {if (error) {return;}}); // Turn on Screen
 
+// start stream - change IP to match RTSP Stream!
+  exec('vlc rtsp://1.1.1.1 --fullscreen', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`)
   
-  res.status(200).json( { Status: 'OK'});
+});
+
+const time = 30; // time after streaming stops
+
+setTimeout(() => {
+    exec('dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+    
+    exec('xset dpms force off', (error, stdout, stderr) => {if (error) {return;}}); // Turn off Screen
+    
+}, time * 1000);
+  
+res.status(200).json( { Status: 'OK'});  
 });
