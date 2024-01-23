a7d85c38a3de9509c314625c315208975431c536
entttom <entner.tom@gmail.com>
2024-01-23 12:39:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 sudo apt update
 sudo apt install nodejs npm net-tools
 npm -i express body-parser pm2 
+git clone https://github.com/entttom/unifi-doorbell-monitor/
+cd unifi-doorbell-monitor
 pm2 start start.js
 pm2 save
 pm2 autostart
-ip addr show | grep 'inet ' | awk '{print "Open the following " $2}'
+ip addr show | grep 'inet ' | awk '{print "Use this as endpoint in Iobroker or Home Asisstant http://" $2"/api/ring_ring"}'
