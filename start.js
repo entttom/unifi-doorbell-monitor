201bed967c61a40a06697a0aee17865903d449b5
entttom <entner.tom@gmail.com>
2024-01-23 15:37:57 +0100

    Update start.js

--- start.js
+++ start.js
@@ -14,17 +14,6 @@ exec('export DISPLAY=:0;xset q;xset dpms force off', (error, stdout, stderr) => 
 
 app.post('/api/ring_ring', (req, res) => {
 
-
-//start vlc, check ifts already running
-exec('if pgrep vlc > /dev/null; then echo "VLC is already running"; else vlc & fi', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-});
-
 exec('xset dpms force on', (error, stdout, stderr) => {if (error) {return;}}); // Turn on Screen
 
 // start stream - change IP to match RTSP Stream!
@@ -32,7 +21,7 @@ exec('xset dpms force on', (error, stdout, stderr) => {if (error) {return;}}); /
   //remove the second s in rtsps
   //change port from 7441 to 7447
   //remove (?enableSrtp)
-rtsp://192.168.1.1:7447/QxJCVMefFHfBnqrp
+
   exec('vlc rtsp://192.168.1.1:7447/6OHQ0QIWgxnIbTTp --fullscreen', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
@@ -46,7 +35,7 @@ rtsp://192.168.1.1:7447/QxJCVMefFHfBnqrp
 const time = 30; // time after streaming stops
 
 setTimeout(() => {
-    exec('dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop', (error, stdout, stderr) => {
+    exec('killall -9 vlc', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
