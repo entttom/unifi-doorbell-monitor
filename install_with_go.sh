3fe1d23b16231f566a3f701684808e4546214194
entttom <entner.tom@gmail.com>
2024-01-24 12:43:57 +0100

    Create install_with_go.sh

---
+++ install_with_go.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install nodejs npm net-tools vlc golang-go gccgo-go -y
+git clone https://github.com/entttom/unifi-doorbell-monitor/
+cd unifi-doorbell-monitor
+sudo npm i -g express body-parser pm2
+go mod init button
+go build
+pm2 start start.js
+pm2 save
+pm2 autostart
+ip addr show | grep 'inet ' | awk '{print "Use this as endpoint in Iobroker or Home Asisstant http://" $2"/api/ring_ring"}'
