1d4194a82129277b87d0911988f4fbed6e8c671e
entttom <entner.tom@gmail.com>
2024-01-25 20:17:07 +0100

    Update install_with_python.py

--- install_with_python.py
+++ install_with_python.py
@@ -4,7 +4,7 @@ sudo apt install nodejs npm net-tools vlc wmctrl -y
 git clone https://github.com/entttom/unifi-doorbell-monitor/
 cd unifi-doorbell-monitor
 sudo npm i -g express body-parser pm2
-pip install python-vlc rpyc PyQt5 webbrowser
+pip install python-vlc rpyc PyQt5 PyQtWebEngine
 pm2 start start.js
 pm2 save
 pm2 autostart
