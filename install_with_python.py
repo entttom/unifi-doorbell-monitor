3dd843c11ecefdaae75e10aaaaf5b74a8f7dd2e2
entttom <entner.tom@gmail.com>
2024-01-26 07:52:32 +0100

    pm2

--- install_with_python.py
+++ install_with_python.py
@@ -5,7 +5,8 @@ git clone https://github.com/entttom/unifi-doorbell-monitor/
 cd unifi-doorbell-monitor
 sudo npm i -g express body-parser pm2
 pip install python-vlc rpyc PyQt5
-pm2 start start.js
-pm2 save
+pm2 start start_with_button_on_side.js --watch
 pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
+pm2 save
 ip addr show | grep 'inet ' | awk '{print "Use this as endpoint in Iobroker or Home Asisstant http://" $2"/api/ring_ring"}'
