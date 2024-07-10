fa3c730562154a62402f659b6724481b5dca978c
entttom <entner.tom@gmail.com>
2024-07-10 20:55:09 +0200

    changes firefox option

--- start_with_button_on_side.js
+++ start_with_button_on_side.js
@@ -21,7 +21,7 @@ const runTimer = () => {
 };
 
 setTimeout(() => {
-exec('firefox --kiosk http://192.168.1.48', (error, stdout, stderr) => {if (error) {return;}}); // Start Firefox 
+exec('firefox --kiosk=http://192.168.1.48', (error, stdout, stderr) => {if (error) {return;}}); // Start Firefox 
 }, "10000"); 
 
 
@@ -71,7 +71,7 @@ exec('pkill -f firefox', (error, stdout, stderr) => {if (error) {return;}});
 res.status(200).json( { Status: 'OK'});  
 });
 app.get('/api/start_browser', (req, res) => {
-exec('export DISPLAY=:0;firefox --kiosk http://192.168.1.48', (error, stdout, stderr) => {if (error) {return;}}); 
+exec('export DISPLAY=:0;firefox --kiosk=http://192.168.1.48', (error, stdout, stderr) => {if (error) {return;}}); 
 res.status(200).json( { Status: 'OK'});  
 });
 app.get('/api/kill_stream_window', (req, res) => {
