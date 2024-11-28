20ccf4f1ef2492bbea68a89db04dbb10e656bb41
entttom <entner.tom@gmail.com>
2024-11-28 10:53:44 +0100

    Update start_with_button_on_side.js

--- start_with_button_on_side.js
+++ start_with_button_on_side.js
@@ -7,6 +7,7 @@ const { exec } = require('node:child_process')
 
 var monitor_on = true;
 var stream = false;
+var stream_front_door = false;
 
 app.use(bodyParser.json());
 
@@ -15,6 +16,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+//Timer for Monitor off and kill streams
 let timer;
 const runTimer = () => {
   timer = setTimeout(() => {
@@ -23,16 +25,19 @@ const runTimer = () => {
     exec('pkill -f stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); // Kill Stream
     monitor_on = false;
     stream = false;
+    stream_front_door = false;
     
   }, "300000"); //Screen auto of after 5 min
 };
 
+//Timer for kill streams only
 let timer_stream;
 const runTimer_stream = () => {
   timer_stream = setTimeout(() => {
     exec('pkill -f stream.py', (error, stdout, stderr) => {if (error) {return;}}); // Kill Stream
     exec('pkill -f stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); // Kill Stream
     stream = false;
+    stream_front_door = false;
     
   }, "300000"); //Screen auto of after 5 min
 };
@@ -74,12 +79,14 @@ app.get('/api/ring_ring', (req, res) => {
       if(stream == false) { 
         exec('python stream.py', (error, stdout, stderr) => {if (error) {return;}}); 
         stream = true;
+        stream_front_door = true;
       };
     }, "2000"); 
     }
     if(monitor_on == true && stream == false) {
       exec('python stream.py', (error, stdout, stderr) => {if (error) {return;}}); 
       stream = true;
+      stream_front_door = true;
     };
     clearTimeout(timer);
     runTimer();
@@ -90,20 +97,32 @@ app.get('/api/ring_ring', (req, res) => {
 
 app.get('/api/front_yard', (req, res) => {
   //exec('export DISPLAY=:0;xset q;xset dpms force on', (error, stdout, stderr) => {if (error) {return;}}); // Turn on Screen Pi3
+  
   if(monitor_on == false) {  
   exec('WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --on', (error, stdout, stderr) => {if (error) {return;}}); // Turn on Screen Pi5
   monitor_on = true;
   setTimeout(() => {
-    if(stream == false) { 
+  if(stream == false) { 
       exec('python stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); 
       stream = true;
     };
   }, "2000"); 
   }
+
   if(monitor_on == true && stream == false) {
     exec('python stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); 
     stream = true;
   };
+
+  if(monitor_on == true && stream_front_door == true) {
+    exec('pkill -f stream.py', (error, stdout, stderr) => {if (error) {return;}}); 
+    setTimeout(() => {
+      exec('python stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); 
+    }, "100"); 
+    stream = true;
+    stream_front_door = false;
+  };
+
   clearTimeout(timer);
   runTimer();
   clearTimeout(timer_stream);
@@ -118,6 +137,7 @@ app.get('/api/stop_streaming_and_turn_off_monitor', (req, res) => {
   exec('WAYLAND_DISPLAY="wayland-1" wlr-randr --output HDMI-A-1 --off', (error, stdout, stderr) => {if (error) {return;}}); // Turn off Screen Pi5 
   monitor_on = false;
   stream = false;
+  stream_front_door = false;
   res.status(200).json( { Status: 'OK'});  
 });
 
@@ -125,7 +145,8 @@ app.get('/api/stop_browser', (req, res) => {
   exec('pkill -f firefox', (error, stdout, stderr) => {if (error) {return;}}); 
   res.status(200).json( { Status: 'OK'});  
   });
-  app.get('/api/start_browser', (req, res) => {
+
+app.get('/api/start_browser', (req, res) => {
   exec('export DISPLAY=:0;firefox --kiosk=http://192.168.1.48', (error, stdout, stderr) => {if (error) {return;}}); 
   res.status(200).json( { Status: 'OK'});  
 });
@@ -134,17 +155,19 @@ app.get('/api/kill_stream_window', (req, res) => {
   exec('pkill -f stream.py', (error, stdout, stderr) => {if (error) {return;}});
   exec('pkill -f stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); // Kill Stream
   stream = false;
+  stream_front_door = false;
   res.status(200).json( { Status: 'OK'});  
 });
 
 app.get('/api/open_stream_window', (req, res) => {
-  stream = false;
+  stream = true;
+  stream_front_door = true;
   exec('python stream.py', (error, stdout, stderr) => {if (error) {return;}}); 
   res.status(200).json( { Status: 'OK'});  
 });
 
 app.get('/api/open_stream_window_front_yard', (req, res) => {
-  stream = false;
+  stream = true;
   exec('python stream_front_yard.py', (error, stdout, stderr) => {if (error) {return;}}); 
   res.status(200).json( { Status: 'OK'});  
 });
@@ -173,7 +196,7 @@ app.get('/api/focus_browser', (req, res) => {
 });  
 
 app.get('/api/debug', (req, res) => {
-  res.status(200).json( { Status: 'OK',Steam: stream, Monitor: monitor_on});  
+  res.status(200).json( { Status: 'OK',Steam: stream, Monitor: monitor_on, FrontDoor: stream_front_door,});  
 });  
 
 
